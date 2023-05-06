893baca7d3682828be18f1b9d2005084d848b04c
FarzadVav <vav.progg.ir@gmail.com>
2023-05-06 22:22:23 +0430

    add z-index to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,14 @@ import "./Header.css"
 export function Header() {
   return (
     <>
-      <div className='bg-blue-500 w-screen h-20 flex justify-between items-center px-5
+      <div className='bg-blue-500 w-screen h-20 flex justify-between items-center px-5 z-50
         sm:px-10'>
         <div className="btn btn-ghost text-xl flex items-center relative lg:hidden group">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-white w-9 h-9 cursor-pointer">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
           </svg>
 
-          <ul className="bg-white shadow-md shadow-[#25252545] w-max flex flex-col justify-center items-center gap-7 p-5 px-3 rounded-xl
+          <ul className="bg-white shadow-md shadow-[#25252545] w-max flex flex-col justify-center items-center gap-7 p-5 px-3 rounded-xl z-50
             absolute top-[140%] right-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible" id='mobileMenu'>
             <li>
               <NavLink className='bg-slate-200 text-blue-500 p-2 px-10 rounded-md'>صفحه اصلی</NavLink>
