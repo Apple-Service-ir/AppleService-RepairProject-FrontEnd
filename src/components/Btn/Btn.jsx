4e62c44e4af6b48acebfbf94b9f85b0fde919f80
FarzadVav <vav.progg.ir@gmail.com>
2023-05-07 18:49:07 +0430

    get file in input

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Btn(props) {
-  console.log(props.bgColor);
   return (
     <Link to={props.href} className={`${props.width} ${props.bgColor} ${props.color} ${props.border}
       flex justify-center items-center gap-2 text-xl rounded-full py-2 px-7
--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -3,18 +3,24 @@ import { useRef } from 'react'
 
 export default function FileInput(props) {
   const inputRef = useRef(null)
+  const nameFileRef = useRef('')
 
   function clickHandler() {
     inputRef.current.click()
   }
 
+  function changeHandler(event) {
+    nameFileRef.current.innerHTML = event.target.files[0].name
+  }
+
   return (
     <div className={`${props.width} h-16 relative`}>
-      <input className='hidden' type="file" ref={inputRef} />
+      <input className='hidden' type="file" ref={inputRef} onChange={changeHandler}
+        accept='.png,.jpg,.jpeg,.heif,.hevc'/>
       <div className="bg-slate-200 border-2 border-slate-300
         w-full h-full flex items-center absolute top-0 left-0 p-3 pl-16 rounded-xl
       	cursor-pointer hover:border-slate-400" onClick={clickHandler}>
-        <span className='text-blue-500 text-sm sm:text-base'>{props.name}</span>
+        <span className='text-blue-500 text-sm sm:text-base' ref={nameFileRef}>{props.name}</span>
       </div>
       <div className='w-10 h-10 flex justify-center items-center absolute left-3 top-1/2 -translate-y-1/2'>
         {props.svg}
