dd6ba931034a77c918e0a92013d445b1c8785946
FarzadVav <vav.progg.ir@gmail.com>
2023-05-10 18:34:42 -0700

    small size in Question component

--- src/components/QuestionItem/QuestionItem.jsx
+++ src/components/QuestionItem/QuestionItem.jsx
@@ -5,7 +5,7 @@ export function QuestionItem(props) {
         <div className="w-[325px] bg-slate-200 shadow-sm shadow-slate-400 flex flex-col justify-center items-center rounded-xl
               sm:w-[425px]">
             <div className="w-full flex justify-between items-center p-3 sm:p-5 pb-3 sm:pb-5">
-                <h6 className="text-xl text-slate-700 sansbold sm:text-2xl">{props.title}</h6>
+                <h6 className="text-xl text-slate-700 sansbold">{props.title}</h6>
                 {props.svg}
             </div>
             <p className="text-slate-500 w-full p-3 pt-0 text-sm sm:text-base sm:p-5 sm:pt-0">
