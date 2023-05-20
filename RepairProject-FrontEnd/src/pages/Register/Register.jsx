6d4618cd7976663c6d4b4149859627794959f8fe
FarzadVav <vav.progg.ir@gmail.com>
2023-05-20 09:06:23 -0700

    add validation in signin

--- RepairProject-FrontEnd/src/pages/Register/Register.jsx
+++ RepairProject-FrontEnd/src/pages/Register/Register.jsx
@@ -6,11 +6,15 @@ import { get, post } from '../../utility'
 
 export default function Register() {
   const [formPage, setFormPage] = useState('login')
-  const [otpForm, setOtpForm] = useState(false)
 
   const [cities, setCities] = useState([])
 
-  const phoneRef = useRef()
+  const loginPhoneRef = useRef()
+
+  const signinNameRef = useRef()
+  const signinLastNameRef = useRef()
+  const signinPhoneRef = useRef()
+  const signinCityRef = useRef()
 
   useEffect(() => {
     get('/list/cities').then(response => {
@@ -19,14 +23,25 @@ export default function Register() {
   }, [])
 
   function submitLogin() {
-    if (phoneRef.current.value.length !== 11 || !phoneRef.current.value.startsWith('09')) {
-      toast.error('لطفا شماره خود را به درستی وارد کنید')
+    if (loginPhoneRef.current.value.length !== 11 || !loginPhoneRef.current.value.startsWith('09')) {
+      toast.error('لطفا شماره خود را به درستی وارد کنید!')
     } else {
-      post('/auth?action=generate', { phone: phoneRef.current.value }).then(response => {
+      post('/auth?action=generate', { phone: loginPhoneRef.current.value }).then(response => {
         console.log(response);
-        // setFormPage('otpPage')
+        setFormPage('otpPage')
       })
     }
+  }
+
+  function submitSignin() {
+    if (
+      signinNameRef.current.value.length < 3
+      || signinLastNameRef.current.value.length < 3
+      || signinPhoneRef.current.value.length !== 11
+      || !signinPhoneRef.current.value.startsWith('09')
+      || signinCityRef.current.value === 'none'
+    ) toast.error('لطفا فیلد هارا به درستی کامل کنید!')
+    else setFormPage('otpPage')
   }
 
   return (
@@ -55,7 +70,7 @@ export default function Register() {
           w-full flex flex-col justify-center items-center gap-3 p-6 show-up`}>
           <div className='w-full bg-input'>
             <input className='input tracking-[0.25rem] text-right' dir='ltr'
-              type="number" placeholder='09*********' ref={phoneRef} />
+              type="number" placeholder='09*********' ref={loginPhoneRef} />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
             </svg>
@@ -68,31 +83,44 @@ export default function Register() {
               onClick={() => setFormPage('signin')}>ثبت نام کنید</span>
           </span>
         </div>
+
         <div className={`${formPage === 'signin' ? 'flex' : 'hidden'}
           w-full flex flex-col justify-center items-center gap-3 p-6 show-up`}>
           <div className='w-full bg-input'>
-            <input className='input'
-              type="text" placeholder='نام' />
+            <input
+              className='input'
+              type="text"
+              placeholder='نام'
+              ref={signinNameRef}
+            />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
           </div>
           <div className='w-full bg-input'>
-            <input className='input'
-              type="text" placeholder='نام خانوادگی' />
+            <input
+              className='input'
+              type="text"
+              placeholder='نام خانوادگی'
+              ref={signinLastNameRef}
+            />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
             </svg>
           </div>
           <div className='w-full bg-input'>
-            <input className='input tracking-[0.25rem] text-right' dir='ltr'
-              type="number" placeholder='*********09' />
+            <input
+              className='input tracking-[0.25rem] text-right' dir='ltr'
+              type="number"
+              placeholder='*********09'
+              ref={signinPhoneRef}
+            />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
             </svg>
           </div>
           <div className='w-full bg-input'>
-            <select className='select-box'>
+            <select className='select-box' ref={signinCityRef}>
               <option value="none">شهرتان را انتخاب کنید</option>
               {
                 cities.map(city => (
@@ -105,7 +133,7 @@ export default function Register() {
             </svg>
           </div>
           <button className='btn btn-blue w-full'
-            onClick={() => setFormPage('otpPage')}>ثبت نام</button>
+            onClick={submitSignin}>ثبت نام</button>
           <span className='text-xs flex justify-center items-center gap-2 mt-1 show-up'>
             حساب کاربری دارید؟
             <span className='text-blue-500 cursor-pointer select-none'
