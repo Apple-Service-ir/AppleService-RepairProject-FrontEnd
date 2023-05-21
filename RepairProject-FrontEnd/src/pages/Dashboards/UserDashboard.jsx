3869dbbd0337c553eb1156d4a9ad2e3b390465a4
FarzadVav <vav.progg.ir@gmail.com>
2023-05-21 12:44:15 -0700

    start developo UserDashboard Page

---
+++ RepairProject-FrontEnd/src/pages/Dashboards/UserDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserDashboard() {
+  return (
+    <div className='w-screen h-screen flex'>
+      <div className="bg-blue-500 w-3/12 h-screen"></div>
+      <div className="bg-white w-9/12 h-screen"></div>
+    </div>
+  )
+}
+
+export default UserDashboard
--- RepairProject-FrontEnd/src/routes.jsx
+++ RepairProject-FrontEnd/src/routes.jsx
@@ -2,6 +2,7 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx"
 import Home from "./pages/Home/Home.jsx"
 import Order from "./pages/Order/Order.jsx"
 import Register from "./pages/Register/Register.jsx"
+import UserDashboard from "./pages/Dashboards/UserDashboard.jsx"
 
 const routes = [
   { path: '/', element: <Home /> },
@@ -10,6 +11,10 @@ const routes = [
     element: <PrivateRoute reDirectPath='/register'> <Order /> </PrivateRoute>
   },
   { path: '/register', element: <Register /> },
+  {
+    path: '/dashboard',
+    element: <PrivateRoute reDirectPath='/dashboard'> <UserDashboard /> </PrivateRoute>
+  },
 ]
 
 export { routes }
