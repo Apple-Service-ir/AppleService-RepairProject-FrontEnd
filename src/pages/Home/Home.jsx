00c6e4268c1eb90d9b6794eac89906bb2d0c93b5
FarzadVav <vav.progg.ir@gmail.com>
2023-05-24 11:33:57 -0700

    change icon in hero section Link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,8 +15,8 @@ export default function Home() {
         <div className="w-full mt-10"></div>
         <Link className="btn btn-white" to='/order'>
           ثبت سفارش
-          <svg className="duration-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-            <path className="duration-0" strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="duration-0 w-5 h-5">
+            <path className="duration-0" strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
           </svg>
         </Link>
       </div>
