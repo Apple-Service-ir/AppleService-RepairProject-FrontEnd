f3c8c4caed0fa1336ced069721ca0db5783d1b87
Wil1i <williams.ca001@gmail.com>
2023-05-31 22:40:15 -0700

    Name updated using auth context | Role authentication for sidebar items added

--- src/components/SideBarLink/SideBarLink.jsx
+++ src/components/SideBarLink/SideBarLink.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import AuthContext from '../../context/AuthContext'
 
-function SideBarLink({ link, title, svg }) {
-  return (
-    <NavLink
-      to={link}
-      className={path =>
-        (path.isActive && location.pathname.endsWith(link))
-          ? 'sidebar-link-active'
-          : 'sidebar-link'
-      }
-    >
-      {title}
-      {svg}
-      <div className='sidebar-circle-t'></div>
-      <span className='sidebar-circle-b'></span>
-    </NavLink>
-  )
+function SideBarLink({ link, title, svg, rank }) {
+  const authContext = useContext(AuthContext)
+
+  if (rank.includes(authContext.userInfo.role)) {
+    return (
+      <NavLink
+        to={link}
+        className={path =>
+          (path.isActive && location.pathname.endsWith(link))
+            ? 'sidebar-link-active'
+            : 'sidebar-link'
+        }
+      >
+        {title}
+        {svg}
+        <div className='sidebar-circle-t'></div>
+        <span className='sidebar-circle-b'></span>
+      </NavLink>
+    )
+  }
 }
 
 export default SideBarLink
