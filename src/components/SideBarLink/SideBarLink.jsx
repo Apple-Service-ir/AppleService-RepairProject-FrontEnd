64f31c43bd8b09fc44d885d269c1924b3336b9ef
FarzadVav <vav.progg.ir@gmail.com>
2023-06-04 10:46:04 -0700

    change admin dashboard sidebar

--- src/components/SideBarLink/SideBarLink.jsx
+++ src/components/SideBarLink/SideBarLink.jsx
@@ -1,27 +1,22 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-import AuthContext from '../../context/AuthContext'
 
-function SideBarLink({ link, title, svg, rank }) {
-  const authContext = useContext(AuthContext)
-
-  if (rank == "all" || rank.includes(authContext.userInfo.role)) {
-    return (
-      <NavLink
-        to={link}
-        className={path =>
-          (path.isActive && location.pathname.endsWith(link))
-            ? 'sidebar-link-active'
-            : 'sidebar-link'
-        }
-      >
-        {title}
-        {svg}
-        <div className='sidebar-circle-t'></div>
-        <span className='sidebar-circle-b'></span>
-      </NavLink>
-    )
-  }
+function SideBarLink({ link, title, svg }) {
+  return (
+    <NavLink
+      to={link}
+      className={path =>
+        (path.isActive && location.pathname.endsWith(link))
+          ? 'sidebar-link-active'
+          : 'sidebar-link'
+      }
+    >
+      {title}
+      {svg}
+      <div className='sidebar-circle-t'></div>
+      <span className='sidebar-circle-b'></span>
+    </NavLink>
+  )
 }
 
 export default SideBarLink
