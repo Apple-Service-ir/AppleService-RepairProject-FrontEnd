ddb5e3f34cdb021567ecfaca062467b0eb747410
FarzadVav <vav.progg.ir@gmail.com>
2023-06-06 13:58:43 -0700

    fixed bug in change status

--- src/pages/Dashboards/AdminOrders.jsx
+++ src/pages/Dashboards/AdminOrders.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { createPortal } from 'react-dom'
+import { Toaster, toast } from 'react-hot-toast'
 
 import { mainUrl } from "../../../config.json"
 import AuthContext from '../../context/AuthContext'
@@ -23,27 +24,31 @@ function AdminOrders() {
       })
   }, [authContext])
 
-  function changeStatusToWorking(orderId) {
-    const currentStatus = 'working'
+  function changeStatus(orderId, currentStatus) {
     const bodyRequest = {
       token: authContext.userToken,
       id: orderId,
       status: currentStatus
     }
     post('/admins/orders/status', bodyRequest)
-      .then(response => {
-        console.log(response);
+      .then(() => {
         setOrders(prev => {
           const newOrders = prev.map(order => {
             if (order.id === orderId) {
               order.status = currentStatus
               return order
             }
+
             return order
           })
+
+          const findOrder = prev.find(order => order.id === orderId)
+          setModal(prev => ({ ...prev, order: findOrder }))
+
           return newOrders
         })
       })
+      .catch(error => toast.error(error.response.data.err))
   }
 
   return (
@@ -129,23 +134,29 @@ function AdminOrders() {
                   className="bg-yellow-500 text-white w-9 h-9
                   flex justify-center items-center rounded-full
                   hover:ring-4 hover:ring-yellow-300"
-                  onClick={() => changeStatusToWorking(modal.order.id)}
+                  onClick={() => changeStatus(modal.order.id, 'working')}
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     strokeWidth={1} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                   </svg>
                 </button>
-                <button className="bg-green-500 text-white w-9 h-9
+                <button
+                  className="bg-green-500 text-white w-9 h-9
                   flex justify-center items-center rounded-full
-                  hover:ring-4 hover:ring-green-300">
+                  hover:ring-4 hover:ring-green-300"
+                  onClick={() => changeStatus(modal.order.id, 'done')}
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                   </svg>
                 </button>
-                <button className="bg-red-500 text-white w-9 h-9
+                <button
+                  className="bg-red-500 text-white w-9 h-9
                   flex justify-center items-center rounded-full
-                  hover:ring-4 hover:ring-red-300">
+                  hover:ring-4 hover:ring-red-300"
+                  onClick={() => changeStatus(modal.order.id, 'cancelled')}
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
@@ -278,6 +289,8 @@ function AdminOrders() {
           document.body
         )
       }
+
+      <Toaster />
     </>
   )
 }
