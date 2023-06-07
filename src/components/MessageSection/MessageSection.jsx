5d97c20f34a8b9ae491ae9dda87fef446127a732
FarzadVav <vav.progg.ir@gmail.com>
2023-06-07 15:15:26 -0700

    add message section

---
+++ src/components/MessageSection/MessageSection.jsx
@@ -0,0 +1,109 @@
+import React, { useContext, useRef, useEffect } from 'react'
+
+import AuthContext from '../../context/AuthContext'
+import { post } from '../../utility'
+
+function MessageSection({ setTickets, ticket, bottomRef }) {
+  const authcontext = useContext(AuthContext)
+  const sendMessageRef = useRef()
+
+  useEffect(() => {
+    bottomRef.current.scrollTop = bottomRef.current.scrollHeight
+  }, [ticket])
+
+  function sendMassageHandler(ticketId) {
+    if (sendMessageRef.current.value.length > 0) {
+      post('/tickets/messages/new',
+        {
+          ticketId,
+          text: sendMessageRef.current.value,
+          token: authcontext.userToken
+        }
+      ).then(response => {
+
+        setTickets((prev) => {
+          const mapedTickets = prev.map(ticket => {
+            if (ticket.id === ticketId) {
+              ticket.messages = response.data.messages
+              return ticket
+            }
+            return ticket
+          })
+
+          return mapedTickets
+        })
+
+        sendMessageRef.current.value = ''
+      }).catch((err) => {
+        toast.error(err.response.data.err)
+      })
+    }
+  }
+
+  return (
+    <div className='bg-white w-full h-full absolute top-0 left-0 rounded-xl
+      flex justify-center items-center'>
+
+      <div className="bg-blue-500 w-full h-12 flex justify-between items-center
+        rounded-t-xl px-6 absolute top-0 left-0">
+        <span className='text-white sansbold'>
+          {ticket.subject}
+        </span>
+        <span className='text-white sansbold'>{ticket.id} #</span>
+      </div>
+
+      <div
+        className="w-full h-full mt-auto overflow-y-scroll px-3 pt-[3.75rem] pb-[4.75rem]"
+        ref={bottomRef}
+      >
+        {
+          ticket.messages ? (
+            ticket.messages.map(message => {
+              return message.isSupport ? (
+                <div key={message.id} className="w-full flex justify-end p-1">
+                  <div className="bg-blue-200 w-max max-w-[80%] p-3 rounded-md relative">
+                    <span className='text-blue-500 text-xs absolute left-2 -top-5'>پشتیبان</span>
+                    <p className='text-sm break-words
+                      sm:text-base'>{message.text}</p>
+                  </div>
+                </div>
+              ) : (
+                <div key={message.id} className="w-full p-1">
+                  <div className="bg-green-200 w-max max-w-[80%] p-3 rounded-md">
+                    <p className='text-sm break-words
+                      sm:text-base'>{message.text}</p>
+                  </div>
+                </div>
+              )
+            })
+          ) : ''
+        }
+      </div>
+
+      <div className="bg-white w-full h-16 flex justify-between items-center gap-3 p-3
+        rounded-b-xl absolute bottom-0 left-0">
+        <button
+          className='bg-blue-500 text-white w-16 h-12 rounded-full
+            flex justify-center items-center'
+          onClick={() => sendMassageHandler(ticket.id)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+            className="w-6 h-6 -rotate-90">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+          </svg>
+        </button>
+        <div className='w-full bg-input'>
+          <input
+            className='input'
+            type="text"
+            placeholder='تایپ کنید'
+            ref={sendMessageRef}
+          />
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default MessageSection
--- src/pages/Dashboards/AdminTickets.jsx
+++ src/pages/Dashboards/AdminTickets.jsx
@@ -1,14 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useEffect } from 'react'
 
 import AuthContext from '../../context/AuthContext'
 import { get } from '../../utility'
-import { useState } from 'react'
+import PortalModal from '../../components/PortalModal/PortalModal'
+import MessageSection from '../../components/MessageSection/MessageSection'
+import { useRef } from 'react'
 
 function AdminTickets() {
   const authContext = useContext(AuthContext)
 
   const [tickets, setTickets] = useState([])
+  const [modal, setModal] = useState({ shwo: false, ticket: {} })
+  const bottomRef = useRef()
 
   useEffect(() => {
     get(`/tickets/all?admin=true&token=${authContext.userToken}`)
@@ -19,59 +23,81 @@ function AdminTickets() {
   }, [])
 
   return (
-    <div className='w-full flex flex-col items-center'>
-      <h1 className='w-full text-right text-xl sansbold'>لیست تیکت ها</h1>
+    <>
+      <div className='w-full flex flex-col items-center'>
+        <h1 className='w-full text-right text-xl sansbold'>لیست تیکت ها</h1>
+        {
+          tickets.length > 0 && (
+            <>
+              <table className='table'>
+                <thead className='thead mt-1'>
+                  <tr className='thead__tr'>
+                    <th className='thead__tr__th w-2/12'>کد تیکت</th>
+                    <th className='thead__tr__th w-2/12'>وضعیت</th>
+                    <th className='thead__tr__th w-6/12'>موضوع تیکت</th>
+                    <th className='thead__tr__th w-2/12'>تاریخ</th>
+                  </tr>
+                </thead>
+                <tbody className='tbody'>
+                  {
+                    tickets.map(ticket => (
+                      <tr
+                        key={ticket.id}
+                        className='tbody__tr cursor-pointer'
+                        onClick={() => {
+                          setModal({ show: true, ticket })
+                        }}
+                      >
+                        <td className='tbody__tr__td w-2/12'>
+                          <div className='w-full flex flex-wrap items-center gap-3 justify-center'>
+                            <button className='badge badge-blue select-text'>{ticket.id} #</button>
+                          </div>
+                        </td>
+                        <td className='tbody__tr__td w-2/12'>
+                          <div className="td__wrapper">
+                            <span className={`text-xs ${ticket.status === 'open' ? 'text-green-500'
+                              : ticket.status === 'closed' ? 'text-red-500'
+                                : ''
+                              }`}>
+                              {
+                                ticket.status === 'open' ? 'پاسخ داده شده'
+                                  : ticket.status === 'closed' ? 'بسته شده'
+                                    : 'در انتظار پاسخ'
+                              }
+                            </span>
+                          </div>
+                        </td>
+                        <td className='tbody__tr__td w-6/12 text-sm'>{ticket.subject}</td>
+                        <td className='tbody__tr__td w-2/12 text-sm'>
+                          {new Date(ticket.createdAt).toLocaleDateString('fa-IR')}
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </>
+          )
+        }
+      </div>
+
       {
-        tickets.length > 0 && (
-          <>
-            <table className='table'>
-              <thead className='thead mt-1'>
-                <tr className='thead__tr'>
-                  <th className='thead__tr__th w-2/12'>کد تیکت</th>
-                  <th className='thead__tr__th w-2/12'>وضعیت</th>
-                  <th className='thead__tr__th w-6/12'>موضوع تیکت</th>
-                  <th className='thead__tr__th w-2/12'>تاریخ</th>
-                </tr>
-              </thead>
-              <tbody className='tbody'>
-                {
-                  tickets.map(ticket => (
-                    <tr
-                      key={ticket.id}
-                      className='tbody__tr cursor-pointer'
-                    >
-                      <td className='tbody__tr__td w-2/12'>
-                        <div className='w-full flex flex-wrap items-center gap-3 justify-center'>
-                          <button className='badge badge-blue select-text'>{ticket.id} #</button>
-                        </div>
-                      </td>
-                      <td className='tbody__tr__td w-2/12'>
-                        <div className="td__wrapper">
-                          <span className={`text-xs ${ticket.status === 'open' ? 'text-green-500'
-                            : ticket.status === 'closed' ? 'text-red-500'
-                              : ''
-                            }`}>
-                            {
-                              ticket.status === 'open' ? 'پاسخ داده شده'
-                                : ticket.status === 'closed' ? 'بسته شده'
-                                  : 'در انتظار پاسخ'
-                            }
-                          </span>
-                        </div>
-                      </td>
-                      <td className='tbody__tr__td w-6/12 text-sm'>{ticket.subject}</td>
-                      <td className='tbody__tr__td w-2/12 text-sm'>
-                        {new Date(ticket.createdAt).toLocaleDateString('fa-IR')}
-                      </td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-          </>
+        modal.show && (
+          <PortalModal
+            closeHandler={() => setModal({ show: false, ticket: {} })}
+          >
+            <div className="w-96 h-[80vh] relative">
+              <MessageSection
+                setTickets={setTickets}
+                ticket={modal.ticket}
+                bottomRef={bottomRef}
+              >
+              </MessageSection>
+            </div>
+          </PortalModal>
         )
       }
-    </div>
+    </>
   )
 }
 
