7bde3e8fb8ede04c1178fb7a61a486801e500ccf
FarzadVav <vav.progg.ir@gmail.com>
2023-06-07 10:47:25 -0700

    redesign change status btns in admin dashboard

--- src/pages/Dashboards/AdminOrders.jsx
+++ src/pages/Dashboards/AdminOrders.jsx
@@ -130,39 +130,42 @@ function AdminOrders() {
         modal.show && createPortal(
           <PortalModal closeHandler={() => setModal({ show: false, order: {} })}>
             <ul className="w-96 max-h-[80vh] overflow-y-scroll rounded-md">
-              <li className='bg-white w-full h-12 rounded-md
-                flex justify-center items-center gap-3'>
+              <li className='w-full rounded-md
+                flex justify-center items-center gap-1'>
                 <button
-                  className="bg-yellow-500 text-white w-9 h-9
-                  flex justify-center items-center rounded-full
-                  hover:ring-4 hover:ring-yellow-300"
-                  onClick={() => changeStatus(modal.order.id, 'working')}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    strokeWidth={1} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
-                  </svg>
-                </button>
-                <button
-                  className="bg-green-500 text-white w-9 h-9
-                  flex justify-center items-center rounded-full
-                  hover:ring-4 hover:ring-green-300"
-                  onClick={() => changeStatus(modal.order.id, 'done')}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                  </svg>
-                </button>
-                <button
-                  className="bg-red-500 text-white w-9 h-9
-                  flex justify-center items-center rounded-full
-                  hover:ring-4 hover:ring-red-300"
+                  className='bg-red-500 text-white w-1/3 h-9 text-sm
+                    py-2 px-6 rounded-md shadow-2xl shadow-black'
                   onClick={() => changeStatus(modal.order.id, 'cancelled')}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
+                  لغو کردن
                 </button>
+                <button
+                  className='bg-yellow-500 text-white w-1/3 h-9 text-sm
+                    py-2 px-6 rounded-md shadow-2xl shadow-black'
+                  onClick={() => changeStatus(modal.order.id, 'working')}
+                >
+                  تایید کردن
+                </button>
+                <button
+                  className='bg-green-500 text-white w-1/3 h-9 text-sm
+                    py-2 px-6 rounded-md shadow-2xl shadow-black'
+                  onClick={() => changeStatus(modal.order.id, 'done')}
+                >
+                  تمام شده
+                </button>
+              </li>
+
+              <li className='w-full flex justify-center items-center rounded-md mt-1'>
+                <div className='w-full bg-textarea'>
+                  <textarea
+                    className='textarea border-0 rounded-md focus:ring-0'
+                    placeholder='توضیحات را وارد کنید'
+                  >
+                  </textarea>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-textarea">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                  </svg>
+                </div>
               </li>
 
               <li className='w-full flex justify-center items-center rounded-md mt-3'>
