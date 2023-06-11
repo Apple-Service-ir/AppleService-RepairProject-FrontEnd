b44c1728299e8807b617a4ba3fdc5233d61731f1
FarzadVav <vav.progg.ir@gmail.com>
2023-06-11 10:34:48 -0700

    add admin brands page

---
+++ src/pages/Dashboards/AdminBrands.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminBrands() {
+  return (
+    <div className='w-full flex flex-col items-center show-fade'>
+      AdminBrands
+    </div>
+  )
+}
+
+export default AdminBrands
