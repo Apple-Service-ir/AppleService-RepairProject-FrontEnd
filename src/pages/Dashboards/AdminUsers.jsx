fe7af41137827acd5c36af20ecd0590b9180b579
Wil1i <williams.ca001@gmail.com>
2023-06-17 12:08:06 -0700

    Input for update user phone number added

--- src/pages/Dashboards/AdminUsers.jsx
+++ src/pages/Dashboards/AdminUsers.jsx
@@ -36,6 +36,7 @@ function AdminUsers() {
     profile: { file: '', validation: true },
     firstName: { value: '', validation: true },
     lastName: { value: '', validation: true },
+    phone: { value: '', validation: true },
     city: { value: '', validation: true },
   })
 
@@ -135,6 +136,7 @@ function AdminUsers() {
       profile: { file: user.profile, validation: true },
       firstName: { value: user.firstName, validation: true },
       lastName: { value: user.lastName, validation: true },
+      phone: { value: user.phone, validation: true },
       city: { value: user.city, validation: true },
     })
     setShowEditInformationModal({ show: true, id: user.id })
@@ -146,7 +148,7 @@ function AdminUsers() {
     if (objWithIdIndex > -1) {
       arr.splice(objWithIdIndex, 1);
     }
-  
+
     return arr;
   }
 
@@ -154,8 +156,8 @@ function AdminUsers() {
     event.preventDefault()
 
     const requestBody = {
-      token : authContext.userToken,
-      id : editInformationForm.id.value
+      token: authContext.userToken,
+      id: editInformationForm.id.value
     }
 
     post("/admins/users/delete", requestBody).then((res) => {
@@ -175,21 +177,22 @@ function AdminUsers() {
           }
         }
       })
-  
+
       setEditInformationForm({
         id: { value: '', validation: true },
         profile: { file: '', validation: true },
         firstName: { value: '', validation: true },
         lastName: { value: '', validation: true },
+        phone: { value: '', validation: true },
         city: { value: '', validation: true },
       })
 
-      setShowEditInformationModal({show : false, id : null})
+      setShowEditInformationModal({ show: false, id: null })
     }).catch((e) => {
       toast.error(e.response.data.err)
     })
   }
-  
+
   const readUrl = file => {
     const reader = new FileReader()
     reader.addEventListener('load', event => {
@@ -539,6 +542,27 @@ function AdminUsers() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                 </svg>
+              </div>
+              <div className='w-full bg-input'>
+                <input
+                  className='input'
+                  type="text"
+                  placeholder='شماره تلفن جدید'
+                  value={editInformationForm.phone.value}
+                  onChange={event => {
+                    setEditInformationForm(prev => ({
+                      ...prev,
+                      phone: {
+                        value: event.target.value,
+                        validation: event.target.value.length >= 3,
+                      }
+                    }))
+                  }}
+                />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg-input">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                </svg>
+
               </div>
               <div className='w-full bg-input'>
                 <select
