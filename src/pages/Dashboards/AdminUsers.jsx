d310c32053d3368fa398b50478f8fa374d3a1ca7
Wil1i <williams.ca001@gmail.com>
2023-06-17 11:53:27 -0700

    Edit user base completed

--- src/pages/Dashboards/AdminUsers.jsx
+++ src/pages/Dashboards/AdminUsers.jsx
@@ -140,6 +140,56 @@ function AdminUsers() {
     setShowEditInformationModal({ show: true, id: user.id })
   }
 
+  const removeUserFromTable = (arr, id) => {
+    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+
+    if (objWithIdIndex > -1) {
+      arr.splice(objWithIdIndex, 1);
+    }
+  
+    return arr;
+  }
+
+  const deleteUserInformationHandler = (event) => {
+    event.preventDefault()
+
+    const requestBody = {
+      token : authContext.userToken,
+      id : editInformationForm.id.value
+    }
+
+    post("/admins/users/delete", requestBody).then((res) => {
+      removeUserFromTable(users, editInformationForm.id.value)
+
+      setUserForm({
+        id: { value: '', validation: false },
+        firstName: { value: '', validation: false },
+        lastName: { value: '', validation: false },
+        phone: { value: '', validation: false },
+        city: { value: '', validation: false },
+        role: {
+          value: 'user', validation: true, buttons: {
+            user: { value: 'user', checked: true },
+            supporter: { value: 'supporter', checked: false },
+            repairman: { value: 'repairman', checked: false },
+          }
+        }
+      })
+  
+      setEditInformationForm({
+        id: { value: '', validation: true },
+        profile: { file: '', validation: true },
+        firstName: { value: '', validation: true },
+        lastName: { value: '', validation: true },
+        city: { value: '', validation: true },
+      })
+
+      setShowEditInformationModal({show : false, id : null})
+    }).catch((e) => {
+      toast.error(e.response.data.err)
+    })
+  }
+  
   const readUrl = file => {
     const reader = new FileReader()
     reader.addEventListener('load', event => {
@@ -514,6 +564,12 @@ function AdminUsers() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
                 </svg>
               </div>
+              <button
+                className="btn btn-danger w-full"
+                onClick={deleteUserInformationHandler}
+              >
+                حذف کاربر
+              </button>
               <button
                 className="btn btn-blue w-full"
                 onClick={changeUserInformationHandler}
