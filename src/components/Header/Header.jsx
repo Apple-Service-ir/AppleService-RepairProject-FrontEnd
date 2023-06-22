a6e9cbb3d7d34c06b88d5f810d363579015613a0
FarzadVav <vav.progg.ir@gmail.com>
2023-06-21 22:44:21 -0700

    change header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,80 +18,82 @@ export function Header() {
 
   return (
     <>
-      <div className='bg-blue-500 w-screen flex justify-between items-center p-3 px-7 z-50'>
-        <div
-          className="btn btn-ghost text-xl flex items-center relative lg:hidden"
-          onClick={() => {
-            showMobileMenu ? setShowMobileMenu(false) : setShowMobileMenu(true)
-          }}
-        >
+      <div className={`${location.pathname === '/' && 'bg-blue-500'} w-screen p-5 z-50 duration-0`}>
+        <div className='bg-blue-500 w-full flex justify-between items-center py-3 px-5 rounded-xl'>
+          <div
+            className="btn btn-ghost text-xl flex items-center relative lg:hidden"
+            onClick={() => {
+              showMobileMenu ? setShowMobileMenu(false) : setShowMobileMenu(true)
+            }}
+          >
+            {
+              showMobileMenu ? (
+                <svg
+                  className="stroke-white w-9 h-9 cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              ) : (
+                <svg
+                  className="stroke-white w-9 h-9 cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+                </svg>
+              )
+            }
+          </div>
+          <ul className='hidden justify-center items-center gap-9 lg:flex'>
+            <li>
+              <NavLink to='/'
+                className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
+                صفحه اصلی
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/order'
+                className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
+                ثبت سفارش
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact-us'
+                className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
+                ارتباط با ما
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/report'
+                className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
+                ثبت شکایات
+              </NavLink>
+            </li>
+          </ul>
           {
-            showMobileMenu ? (
-              <svg
-                className="stroke-white w-9 h-9 cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
+            authcontext.isLogin ? (
+              <NavLink
+                className={link => link.isActive ? 'btn btn-white' : 'btn btn-out-white'}
+                to='/dashboard'
               >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+                {authcontext.userInfo.firstName} {authcontext.userInfo.lastName}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="duration-0 w-5 h-5">
+                  <path className="duration-0" strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+              </NavLink>
             ) : (
-              <svg
-                className="stroke-white w-9 h-9 cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-              </svg>
+              <NavLink className='btn btn-out-white' to='/register'>ثبت نام / ورورد</NavLink>
             )
           }
         </div>
-        <ul className='hidden justify-center items-center gap-9 lg:flex'>
-          <li>
-            <NavLink to='/'
-              className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
-              صفحه اصلی
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/order'
-              className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
-              ثبت سفارش
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/contact-us'
-              className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
-              ارتباط با ما
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/report'
-              className={link => link.isActive ? 'header__navlink-active' : 'header__navlink'}>
-              ثبت شکایات
-            </NavLink>
-          </li>
-        </ul>
-        {
-          authcontext.isLogin ? (
-            <NavLink
-              className={link => link.isActive ? 'btn btn-white' : 'btn btn-out-white'}
-              to='/dashboard'
-            >
-              {authcontext.userInfo.firstName} {authcontext.userInfo.lastName}
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="duration-0 w-5 h-5">
-                <path className="duration-0" strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-              </svg>
-            </NavLink>
-          ) : (
-            <NavLink className='btn btn-out-white' to='/register'>ثبت نام / ورورد</NavLink>
-          )
-        }
-      </div>
+      </div >
 
       <div
         className={`w-screen h-screen fixed top-0 left-0 z-50
