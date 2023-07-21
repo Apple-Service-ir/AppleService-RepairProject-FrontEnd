6f019e0ee7b68c1c448a733e4bda3258b027ff6a
FarzadVav <vav.progg.ir@gmail.com>
2023-07-21 08:44:19 -0700

    rename imgs folder to images

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 function NotFound() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
+      <img
+        src="images/poz.png"
+        alt="404 not found"
+      />
       404 | Not found
     </div>
   )
