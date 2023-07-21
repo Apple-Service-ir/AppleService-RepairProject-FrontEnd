6c95aa9c7b0d01896a0669f4ed3e499b74a3f6b0
FarzadVav <vav.progg.ir@gmail.com>
2023-07-21 08:49:19 -0700

    design not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
       <img
-        src="images/poz.png"
+        className='w-96 h-48 object-cover'
+        src="./../../../public/images/not-found.png"
         alt="404 not found"
       />
-      404 | Not found
+      <h1>
+        صفحه مورد نطر پیدا نشد
+      </h1>
+      <a
+        className='text-blue-500 underline mt-6'
+        href="/"
+      >
+        بازگشت به سایت
+      </a>
     </div>
   )
 }
