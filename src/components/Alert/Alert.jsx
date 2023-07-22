c0e3c9ccd0b42c7306810e252dd633a327fc8130
FarzadVav <vav.progg.ir@gmail.com>
2023-07-22 10:53:23 -0700

    change style in alert component

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -5,9 +5,16 @@ function Alert({ theme, title, link, linkTitle, icon }) {
   return (
     <div className={theme === 'danger' ? 'alert-danger' : ''}>
       <div className='flex justify-center items-center gap-3'>
-        <h1 className='text-xl'>{title}</h1>
+        <h1 className='text-base sm:text-xl'>{title}</h1>
         {
-          (link && linkTitle) && (<Link to={link} className='text-black underline text-sm'>{linkTitle}</Link>)
+          (link && linkTitle) && (
+            <Link
+              className='text-black underline text-sm'
+              to={link}
+            >
+              {linkTitle}
+            </Link>
+          )
         }
       </div>
       {
