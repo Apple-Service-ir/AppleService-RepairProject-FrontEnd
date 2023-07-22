27fa492e31f03e19f9808baf684f2f165b897d9f
FarzadVav <vav.progg.ir@gmail.com>
2023-07-22 13:55:06 -0700

    fixed bug on accept order in repairmanGetOrder page

--- src/pages/Dashboards/RepairManGetOrder.jsx
+++ src/pages/Dashboards/RepairManGetOrder.jsx
@@ -63,8 +63,7 @@ function RepairManGetOrder() {
 
     const requestBody = {
       id: orderId,
-      price: +priceInputRef.current.value,
-      ...(hasPrice && { status: 'payment-working' })
+      ...(hasPrice && { status: 'payment-working', price: +priceInputRef.current.value })
     }
     await post('/repairmans/orders/accept', authContext.userToken, requestBody)
       .then(response => {
@@ -306,45 +305,47 @@ function RepairManGetOrder() {
           ) : ''
         }
         <div className="w-full overflow-x-auto">
-          <h1 className='w-full text-right text-xl sansbold mb-3'>سفارش های در انتظار تایید</h1>
           {
             orders.pending.length > 0 ? orders.pending.map(order => (
-              <ul key={order.id} className="bg-green-200 w-[750px] h-12
+              <div className="w-full">
+                <h1 className='w-full text-right text-xl sansbold mb-3'>سفارش های در انتظار تایید</h1>
+                <ul key={order.id} className="bg-green-200 w-[750px] h-12
                 flex justify-between items-center
                 pr-6 rounded-xl relative mb-3 sm:w-full">
-                <span className='px-2 text-sm opacity-90'>{order.id} #</span>
-                <li>
-                  نام دستگاه:
-                  <span className='px-2 text-sm opacity-90'>{order.phoneName}</span>
-                </li>
-                <li>
-                  نام قطعه:
-                  <span className='px-2 text-sm opacity-90'>{order.partName}</span>
-                </li>
-                <li>
-                  تصویر:
-                  <a
-                    href={`${baseURL}/uploads/${order.picture}`}
-                    className='px-2 underline text-sm opacity-90'
-                    target="_blank"
-                  >
-                    مشاهده
-                  </a>
-                </li>
-                <button
-                  className='bg-green-300 w-16 h-full flex justify-center items-center rounded-l-xl
+                  <span className='px-2 text-sm opacity-90'>{order.id} #</span>
+                  <li>
+                    نام دستگاه:
+                    <span className='px-2 text-sm opacity-90'>{order.phoneName}</span>
+                  </li>
+                  <li>
+                    نام قطعه:
+                    <span className='px-2 text-sm opacity-90'>{order.partName}</span>
+                  </li>
+                  <li>
+                    تصویر:
+                    <a
+                      href={`${baseURL}/uploads/${order.picture}`}
+                      className='px-2 underline text-sm opacity-90'
+                      target="_blank"
+                    >
+                      مشاهده
+                    </a>
+                  </li>
+                  <button
+                    className='bg-green-300 w-16 h-full flex justify-center items-center rounded-l-xl
                   hover:bg-green-400'
-                  onClick={() => {
-                    setSubmitOrderModal({ show: true, order })
-                  }}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                    className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-                </button>
-              </ul>
+                    onClick={() => {
+                      setSubmitOrderModal({ show: true, order })
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                      className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </button>
+                </ul>
+              </div>
             )) : (
               <Alert
                 theme={'danger'}
