e950513200f58ea40c8b35324a2c0dbbb4e65569
FarzadVav <vav.progg.ir@gmail.com>
2023-07-23 07:02:13 -0700

    memoize admin devices page

--- src/pages/Dashboards/AdminDevices.jsx
+++ src/pages/Dashboards/AdminDevices.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext, useMemo } from 'react'
 import { toast, Toaster } from 'react-hot-toast'
 
 import { get, post } from '../../utils/connection'
@@ -213,80 +213,85 @@ function AdminDevices() {
         }
 
         {
-          datas.devices.length > 0 ? (
-            <div className='w-full mt-6'>
-              <h1 className='w-full text-right text-xl sansbold'>
-                لیست دستگاه ها
-              </h1>
-              <div className="w-full overflow-x-auto rounded-xl mt-3">
-                <table className='table'>
-                  <thead className='thead'>
-                    <tr className='thead__tr'>
-                      <th className='thead__tr__th w-2/12'>ID</th>
-                      <th className='thead__tr__th w-4/12'>برند</th>
-                      <th className='thead__tr__th w-4/12'>مدل</th>
-                      <th className='thead__tr__th w-1/12'>ویرایش</th>
-                      <th className='thead__tr__th w-1/12'>حذف</th>
-                    </tr>
-                  </thead>
-                  <tbody className='tbody'>
-                    {
-                      datas.devices.map(device => (
-                        <tr
-                          key={device.id}
-                          className='tbody__tr'
-                        >
-                          <td className='tbody__tr__td w-2/12'>
-                            <div className='w-full flex flex-wrap items-center gap-3 justify-center'>
-                              <button className='badge badge-blue select-text'>{device.id} #</button>
-                            </div>
-                          </td>
-                          <td className='tbody__tr__td w-4/12 ltr text-right'>{device.brand}</td>
-                          <td className='tbody__tr__td w-4/12 text-sm ltr text-right'>{device.model}</td>
-                          <td
-                            className='tbody__tr__td w-1/12 group cursor-pointer'
-                            onClick={() => {
-                              setEditDeviceModal(
-                                { show: true, id: device.id, brand: device.brand, model: device.model }
-                              )
-                            }}
+          useMemo(() => {
+            return datas.devices.length > 0 ? (
+              <div className='w-full mt-6'>
+                {
+                  console.log(123)
+                }
+                <h1 className='w-full text-right text-xl sansbold'>
+                  لیست دستگاه ها
+                </h1>
+                <div className="w-full overflow-x-auto rounded-xl mt-3">
+                  <table className='table'>
+                    <thead className='thead'>
+                      <tr className='thead__tr'>
+                        <th className='thead__tr__th w-2/12'>ID</th>
+                        <th className='thead__tr__th w-4/12'>برند</th>
+                        <th className='thead__tr__th w-4/12'>مدل</th>
+                        <th className='thead__tr__th w-1/12'>ویرایش</th>
+                        <th className='thead__tr__th w-1/12'>حذف</th>
+                      </tr>
+                    </thead>
+                    <tbody className='tbody'>
+                      {
+                        datas.devices.map(device => (
+                          <tr
+                            key={device.id}
+                            className='tbody__tr'
                           >
-                            <div className="td__wrapper justify-center">
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 w-5 h-5
-                            group-hover:-translate-y-1">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                              </svg>
-                            </div>
-                          </td>
-                          <td
-                            className='tbody__tr__td w-1/12 group cursor-pointer'
-                            onClick={() => {
-                              setDeleteDeviceLoading({ isLoading: true, id: device.id })
-                              removeDevice(device.id)
-                            }}
-                          >
-                            <div className="td__wrapper justify-center">
-                              <DeleteIconLoader
-                                customClass={'group-hover:-translate-y-1'}
-                                isLoading={deleteDeviceLoading.isLoading && (deleteDeviceLoading.id === device.id)}
-                              />
-                            </div>
-                          </td>
-                        </tr>
-                      ))
-                    }
-                  </tbody>
-                </table>
+                            <td className='tbody__tr__td w-2/12'>
+                              <div className='w-full flex flex-wrap items-center gap-3 justify-center'>
+                                <button className='badge badge-blue select-text'>{device.id} #</button>
+                              </div>
+                            </td>
+                            <td className='tbody__tr__td w-4/12 ltr text-right'>{device.brand}</td>
+                            <td className='tbody__tr__td w-4/12 text-sm ltr text-right'>{device.model}</td>
+                            <td
+                              className='tbody__tr__td w-1/12 group cursor-pointer'
+                              onClick={() => {
+                                setEditDeviceModal(
+                                  { show: true, id: device.id, brand: device.brand, model: device.model }
+                                )
+                              }}
+                            >
+                              <div className="td__wrapper justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 w-5 h-5
+                              group-hover:-translate-y-1">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                </svg>
+                              </div>
+                            </td>
+                            <td
+                              className='tbody__tr__td w-1/12 group cursor-pointer'
+                              onClick={() => {
+                                setDeleteDeviceLoading({ isLoading: true, id: device.id })
+                                removeDevice(device.id)
+                              }}
+                            >
+                              <div className="td__wrapper justify-center">
+                                <DeleteIconLoader
+                                  customClass={'group-hover:-translate-y-1'}
+                                  isLoading={deleteDeviceLoading.isLoading && (deleteDeviceLoading.id === device.id)}
+                                />
+                              </div>
+                            </td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className='w-full mt-3'>
-              <Alert
-                theme={'danger'}
-                title={'دستگاهی ثبت نشده است'}
-              />
-            </div>
-          )
+            ) : (
+              <div className='w-full mt-3'>
+                <Alert
+                  theme={'danger'}
+                  title={'دستگاهی ثبت نشده است'}
+                />
+              </div>
+            )
+          }, [datas])
         }
       </div >
 
